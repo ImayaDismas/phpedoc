5ba666d364f97ebc120cbc8243cc8b5ca2a86597
root <root@localhost.localdomain>
2017-06-26 19:21:58 +0300

    added all the code for the api

--- chores/Source Files/include/DbHandler.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 6/26/17
- * Time: 5:14 PM
- */
---
+++ chores/include/Config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/26/17
+ * Time: 5:13 PM
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'nyagaka2013');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'task_manager');
+
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
