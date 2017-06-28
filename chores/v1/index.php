ae4678efef9d718e105fbdcc34838acbae96f8cd
root <imayadismas@gmail.com>
2017-06-28 20:56:09 +0300

    added the swift transfer option on test

--- chores/v1/index.php
+++ chores/v1/index.php
@@ -317,7 +317,7 @@ function verifyRequiredParams($required_fields) {
  */
 function validateEmail($email) {
     $app = \Slim\Slim::getInstance();
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var(trim(strtolower($email)), FILTER_VALIDATE_EMAIL)) {
         $response["error"] = true;
         $response["message"] = 'Email address is not valid';
         echoRespnse(400, $response);
